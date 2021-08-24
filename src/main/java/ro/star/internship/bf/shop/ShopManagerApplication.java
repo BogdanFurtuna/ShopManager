9c331c2a83e364c0766700d613c35c8564b00a56
BogdanFurtuna <b.furtuna@yahoo.com>
2021-08-24 10:25:05 +0100

    Finished RESTful HTTP requests. Migrated the loading code to bootsrap package.

--- src/main/java/ro/star/internship/bf/shop/ShopManagerApplication.java
+++ src/main/java/ro/star/internship/bf/shop/ShopManagerApplication.java
@@ -1,49 +1,24 @@
 package ro.star.internship.bf.shop;
 
-import ro.star.internship.bf.shop.*;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ro.star.internship.bf.shop.model.Category;
-import ro.star.internship.bf.shop.model.Product;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import static ro.star.internship.bf.shop.bootstrap.BootStrapData.loadData;
 
 @SpringBootApplication
 public class ShopManagerApplication {
 
     private static Logger logger = LogManager.getLogger(ShopManagerApplication.class);
-    private static final LinkedHashSet<Category> categoryList = new LinkedHashSet<>();
-    private static final ArrayList<Product> productList = new ArrayList<>();
 
 
     public static void main(String[] args) {
-        Category c1 = new Category("FOOD");
-        Category c2 = new Category("CLOTHES");
-        categoryList.add(c1);
-        categoryList.add(c2);
-
-        Product p1 = new Product("MILK", 10, 5, 100);
-        Product p2 = new Product("BREAD", 8, 7, 100);
-        productList.add(p1);
-        productList.add(p2);
-        c1.setProductList(productList);
-
-        Product p3 = new Product("PANTS", 5,6,100);
-        ArrayList<Product> prodList = new ArrayList<>();
-        prodList.add(p3);
-        c2.setProductList(prodList);
+        logger.trace("Application started!");
+        loadData();
 
         SpringApplication.run(ShopManagerApplication.class, args);
+        logger.trace("Application ended!");
     }
 
-    public static LinkedHashSet<Category> getCategoryList() {
-        return categoryList;
-    }
-
-    public static ArrayList<Product> getProductList() {
-        return productList;
-    }
 }
